8966a1f6ce27d6028f10c521a33f17fb22be2150
Abisha Mochamad Abisha Valbie <abisha.mochamad@student.vinci.be>
2021-11-23 10:28:53 +0100

    added world bound

--- src/Components/Game/GameScene.js
+++ src/Components/Game/GameScene.js
@@ -1,8 +1,9 @@
 import Phaser from "phaser";
-const PLAYER_KEY = "dude";
+const PLAYER_KEY = "player";
+const BULLET = "bullet";
 import skyAsset from "../../assets/sky.png";
 import playerAsset from "../../assets/star.png";
-
+import bulletAsset from "../../assets/bomb.png";
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -10,17 +11,28 @@ class GameScene extends Phaser.Scene {
     this.player = undefined;
     this.cursors = undefined;
     this.gameOver = false;
+    this.bullets = undefined;
   }
   preload() {
     this.load.image("sky", skyAsset);
     this.load.image(PLAYER_KEY,playerAsset);
+    this.load.image(BULLET,bulletAsset);
   }
 
 
   //Load player and background
   create(){
+
+    this.bullets = game.add.group();
+    /*this.bullets.enableBody = true;
+    this.bullets.createMultuple(30, BULLET);
+    this.bullets.setAll('anchor.x', 0.5);
+    this.bullets.setAll('anchor.y', 1);
+    this.bullets.setAll('outOfBoundsKill', true);
+    this.bullets.setAll('checkWorldBounds', true);*/
     this.add.sprite(400, 300, "sky");
     this.player = this.createPlayer();
+
     this.cursors = this.input.keyboard.createCursorKeys();
 
   
