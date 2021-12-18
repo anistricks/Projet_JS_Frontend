302963e7c1c3f438422054101ca3db1c41246dd3
ASSAIDI ANIS <anis.assaidi@student.vinci.be>
2021-12-18 10:31:18 +0100

    add navbar authentification

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,7 @@
 // Here, because our JS component 'Navbar' has the same name as Navbar Bootstrap's component
 // we change the name of the imported Bootstrap's 'Navbar' component
 import { Navbar as BootstrapNavbar} from "bootstrap";
-
+import { getSessionObject } from "../../utils/session";
 /**
  * Render the Navbar which is styled by using Bootstrap
  * Each item in the Navbar is tightly coupled with the Router configuration :
@@ -13,10 +13,15 @@ import { Navbar as BootstrapNavbar} from "bootstrap";
 
 const Navbar = () => {
   const navbarWrapper = document.querySelector("#navbarWrapper");
-  let navbar = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  let navbar;
+  // Get the user object from the localStorage
+  let user = getSessionObject("user");
+
+if (!user) {
+   navbar = `
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Add your brand here</a>
+          <a class="navbar-brand" href="#">Space Defender</a>
           <button
             class="navbar-toggler"
             type="button"
@@ -37,13 +42,54 @@ const Navbar = () => {
                 <a class="nav-link" href="#" data-uri="/game">Game</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#" data-uri="/new">New Page</a>
-              </li>                        
+                <a class="nav-link" href="#" data-uri="/login">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" data-uri="/register">Register</a>
+              </li>
+                                     
             </ul>
           </div>
         </div>
       </nav>
   `;  
+}else{
+  navbar = `
+  <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Space Defendere</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#" data-uri="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" data-uri="/game">Game</a>
+              </li>
+              <li class="nav-item">
+              <a class="nav-link" href="#" data-uri="/logout">Logout</a>
+            </li>
+              <li class="nav-item">
+              <a class="nav-item nav-link disabled" href="#">${user.username}</a>
+              </li>                    
+            </ul>
+          </div>
+        </div>
+      </nav>
+  `;  
+
+}
   navbarWrapper.innerHTML = navbar;
 };
 
